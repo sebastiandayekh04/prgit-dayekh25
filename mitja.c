49abf077871d8c4e8deba4d904a00c95345a8b7d
sebastiandayekh04 <dayekhmartinezsebastianjesus@fpllefia.com>
2024-10-01 16:25:47 +0200

    prgit-dayekh25 v1

---
+++ mitja.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	float suma=0;
+	float mitja;
+	float nou_valor;
+	int numero_repeticions=0;
+	char consulta='s';
+	
+	while(consulta=='s') {
+		printf(" Dayekh. Dòna la següent dada: ");
+		scanf("%f",&nou_valor);
+		suma=suma+nou_valor;
+		numero_repeticions=numero_repeticions+1;
+		printf("Dayekh. Vols continuar (s/n): ");
+		scanf(" %c",&consulta); 
+	}
+	mitja=suma/numero_repeticions;
+	printf(" Dayekh. La mitja és: %.2f\n",mitja);	
+	return 0;
+}
+
